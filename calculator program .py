1e64045175d5d6d1fc7e253f1e267b25a7db3ac7
obeddsouza31-hash <obeddsouza31@gmail.com>
2026-09-17 15:30:34 +0530

    Add basic calculator functions for addition and subtraction

---
+++ calculator program .py
@@ -0,0 +1,13 @@
+def add(a, b ):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
