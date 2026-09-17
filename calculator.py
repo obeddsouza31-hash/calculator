ea23b2a41f43037e2722e133a745df42d548b496
unknown <obeddsouza31@gmail.com>
2026-09-17 15:55:15 +0530

    add multiplication and division functionality

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-def add(a, b ):
+def add(a, b):
     return a + b
 
 
@@ -6,8 +6,20 @@ def subtract(a, b):
     return a - b
 
 
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
+
+
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 print("Addition:", add(num1, num2))
 print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
